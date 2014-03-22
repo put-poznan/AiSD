d6be51274e0520f219d0f9823e125f4cd10487de
Xevaquor <xevaquor@hotmail.com>
2014-03-22 17:46:24 +0100

    shell with knuth

--- shell.py
+++ shell.py
@@ -6,20 +6,23 @@ def ShellSort(a):
 	p = 0
 	gap = 1
 	tmp = 0
+	k = 1
 
-
-	gap = N / 2
-	while gap > 0:
+	gap = 1
+	while 0 < gap < N:
 		for p in range(gap, N):
 			tmp = a[p]
 			j = p
 			while j >= gap and tmp < a[j - gap]:
 				a[j] = a[j - gap]
 
-				j = j - gap
+				j -= gap
 
 			a[j] = tmp
-		gap /= 2
+		k += 1
+		gap = ((3 ** k) - 1) / 2
+
+
 
 
 dd = [4, 3, 6, 5, 4, -10, 15, -45, 3, 8]
